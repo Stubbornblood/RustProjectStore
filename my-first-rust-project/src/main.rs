5999b4c91a9deb841d404ab904e8bead62127f20
Hare Krishna <shivanshu singh>
2024-02-25 16:38:36 +0530

    income divider app completed

--- my-first-rust-project/src/main.rs
+++ my-first-rust-project/src/main.rs
@@ -1,13 +1,27 @@
 slint::include_modules!();
 
+const TAXPER : f64 = 0.30;
+const OWNERPER : f64 = 0.55;
+const PROFITPER : f64 = 0.05;
+const OPEXPER : f64 = 0.10; 
+
+
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_divide_income({
         let ui_handle = ui.as_weak();
-        move || {
+        move |String| {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let num:f64 = String.trim().parse().unwrap();
+            let tax:f64 = num * TAXPER;
+            let owner:f64 = num * OWNERPER;
+            let profit:f64 = num * PROFITPER;
+            let opex:f64 = num * OPEXPER;
+            let result:String = format!("Taxes: {:.2}\nOwner:{:.2}\nProfit:{:.2}\nOpEx:{:.2}",tax,owner,profit,opex);
+     
+            ui.set_results(result.into());
         }
     });
 
